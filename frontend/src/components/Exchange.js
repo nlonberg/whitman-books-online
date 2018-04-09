a4c14592bfc37b1e4a88935261f7b86a234a2097
Nikhil Lonberg <lonbern@whitman.edu>
2018-04-09 14:32:15 -0700

    preliminary Sort By component under the search bar

--- frontend/src/components/Exchange.js
+++ frontend/src/components/Exchange.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Page from './Page';
 import BookFeed from './BookFeed';
 import SearchBar from 'material-ui-search-bar';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 class Exchange extends Component {
   constructor(props) {
@@ -25,6 +27,19 @@ class Exchange extends Component {
             maxWidth: 800
           }}
         />
+
+        <div>
+          <SelectField
+            floatingLabelText="Sort By"
+            value={this.state.value}
+            onChange={(value) => this.setState({value})}
+            >
+            <MenuItem value={1} primaryText="Price: Low to High" />
+            <MenuItem value={2} primaryText="Price: High to Low" />
+            <MenuItem value={3} primaryText="Condition: New to Poor" />
+            <MenuItem value={4} primaryText="Condition: Poor to New" />
+          </SelectField>
+        </div>
 
         <BookFeed query={{}} />
       </Page>
